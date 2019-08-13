b7987be8f90c83d12c3e616016173344df9bb897
Manuel <manuel1412@gmail.com>
2019-08-13 16:06:08 +0100

    Fixes #25 and closes #26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Algorithms Benchmark App';
+  title = 'Algorithm Benchmarking App';
 }
